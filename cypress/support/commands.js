43ef02cde7c21fd8d3e3b9dc8b949d8b0f6b8a09
Xeniya Shirinova <xshirinova@auvik.com>
2020-11-08 13:16:38 -0500

    code cleanup

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+//There are a lot of dropdowns with the same functionality but different data set
+//so dpordown check can be used in many tests for different pages
 Cypress.Commands.add("dropdownCheck", (webElement, expectedList) => {
   cy.get(webElement)
       .then(el => {
